10dedfab929a0769ddfc5685c90ad5085d57d64f
Adam Piper <adam@ahri.net>
2010-06-28 13:07:58 +0100

    initial version of script to write symbolic links to dotfiles

---
+++ write_links.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+dir="`dirname "$0"`"
+
+find "$dir" -maxdepth 1 | while read file; do
+
+        case "$file" in
+        "$dir"|"$dir/.git"|"$dir/.gitignore"|"$dir/README"|*.swp|"$0")
+                continue
+                ;;
+        esac
+
+        rm -rf "$HOME/`basename $file`"
+        ln -s "$file" "$HOME"
+done
