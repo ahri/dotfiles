1aea7dd12c67f3c84d277ecc71478bdf3885a672
Adam Piper <adam@ahri.net>
2015-01-06 19:13:51 +0000

    Add more packages to bootstrap.

--- bootstrap-new-linux.sh
+++ bootstrap-new-linux.sh
@@ -2,7 +2,7 @@
 
 set -ue
 
-packages="git ruby rake rxvt-unicode i3 i3status"
+packages="git gvim vim ruby rake rxvt-unicode i3 i3status unclutter"
 
 pkgmgr="`(which apt-get || which yum) 2> /dev/null || (echo "No supported package manager found" 1>&2 && false)`"
 
