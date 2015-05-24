b2bc966f5545e13210c32a93604169ba5138ae79
Adam Piper <adam@ahri.net>
2015-05-24 14:09:24 +0100

    Organise packages a little more.

--- bootstrap-new-linux.sh
+++ bootstrap-new-linux.sh
@@ -2,7 +2,13 @@
 
 set -ue
 
-packages="git vim ruby rake rxvt-unicode i3 i3status unclutter dtrx htop"
+misc="dtrx htop"
+build="rake build-essential cmake git"
+languages="python python-dev ruby"
+console_dev="vim dvtm abduco"
+windowing="i3 i3status unclutter rxvt-unicode"
+
+packages="$misc $languages $console_dev $windowing"
 params=""
 
 pkgmgr="`(which apt-get || which yum) 2> /dev/null || (echo "No supported package manager found" 1>&2 && false)`"
