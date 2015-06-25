3e32a01d936c21a407f85df38cb19680ba9cabe1
Adam Piper <adam@ahri.net>
2015-06-25 10:32:40 +0100

    Tweak rsync.

--- per-machine/work-dev.sh
+++ per-machine/work-dev.sh
@@ -2,5 +2,5 @@
 
 set -ue
 
-rsync --archive --delete --include '*/' --include '**/.git/**' --exclude '*' --prune-empty-dirs /Volumes/AdamVmHome/.vim ~/
+rsync -rt --delete --include '*/' --include '**/.git/**' --exclude '*' --prune-empty-dirs /Volumes/AdamVmHome/.vim ~/
 find ~/.vim -type d -name .git -exec bash -c "cd {} && cd .. && pwd && git checkout ." \;
