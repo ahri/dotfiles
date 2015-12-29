90c350397e9701f77b03ed77a52e7d487718a106
Adam Piper <adam@ahri.net>
2015-12-29 09:23:48 +0000

    Use ssh instead of mounted dirs.

--- per-machine/work-dev.sh
+++ per-machine/work-dev.sh
@@ -2,5 +2,5 @@
 
 set -ue
 
-rsync -rt --delete --include '*/' --include '**/.git/**' --exclude '*' --prune-empty-dirs /Volumes/AdamVmHome/.vim ~/
+rsync -rt --delete --include '*/' --include '**/.git/**' --exclude '*' --prune-empty-dirs -e 'ssh -l user' linux:.vim ~/
 find ~/.vim -type d -name .git -exec bash -c "cd {} && cd .. && pwd && git checkout ." \;
