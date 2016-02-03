e2fb982fce52a8a5cb80767273367799effa71b8
Adam Piper <adam@ahri.net>
2016-02-03 07:37:00 +0000

    Keep every weird environmental quirk in i3-etc. Consider renaming.

--- bin/natural-scrolling.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -ue
-
-device=`xinput list | grep 'Synaptics' | sed 's,.*id=,,;s,\t.*,,'`
-prop=`xinput list-props $device | grep 'Synaptics Scrolling Distance' | sed 's,.*(,,;s,).*,,'`
-
-xinput set-prop $device $prop -106 -106
