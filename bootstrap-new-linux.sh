b7c4807d912129d586798d06ab266a1bf5bdcdb7
Adam Piper <adam@ahri.net>
2016-10-24 16:03:02 +0100

    Update bootstrap.

--- bootstrap-new-linux.sh
+++ bootstrap-new-linux.sh
@@ -2,24 +2,15 @@
 
 set -ue
 
-misc="dtrx htop curl wget silversearcher-ag xclip"
-build="rake build-essential cmake git openjdk-8-jdk"
-languages="python python-dev ruby"
+misc="htop curl wget xclip lshw"
+build="build-essential cmake git openjdk-8-jdk"
+languages="python python-dev ruby ruby-dev"
 console_dev="vim dvtm"
-windowing="i3 i3status unclutter rxvt-unicode-256color"
+windowing="i3 i3status unclutter qterminal"
 
 packages="$misc $build $languages $console_dev $windowing"
 params=""
 
 pkgmgr="`(which apt-get || which yum) 2> /dev/null || (echo "No supported package manager found" 1>&2 && false)`"
-
-case $pkgmgr in
-*/apt-get)
-    packages="$packages vim-gtk"
-    params="-y"
-    ;;
-*/yum)
-    packages="$packages gvim"
-esac
 
 $pkgmgr install $params $packages
