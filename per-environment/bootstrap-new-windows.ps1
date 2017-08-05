db732d5fa85d61db0e36b1ce1b667314b71dfbf6
Adam Piper <adam@ahri.net>
2017-08-05 11:50:36 +0100

    Move bootstrapping scripts to new env dir.

---
+++ per-environment/bootstrap-new-windows.ps1
@@ -0,0 +1,9 @@
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+scoop bucket add extras
+scoop install 7zip vim openssh git grep touch head tail curl wget gawk concfg pshazz atom stack python ruby nodejs nmap msys2
+[environment]::setenvironmentvariable('GIT_SSH', (resolve-path (scoop which ssh)), 'USER')
+gem install rake
+npm install -g elm
+stack install stylish-haskell ghc-mod
+apm install language-haskell haskell-ghc-mod ide-haskell-cabal ide-haskell autocomplete-haskell
+concfg import monokai small
