57e697f27b678ad3b61f4d6df17ffef746d134a6
Adam Piper <adam@ahri.net>
2017-08-24 22:00:15 +0100

    Pare down installed packages a little more.

--- per-environment/bootstrap-new-windows.ps1
+++ per-environment/bootstrap-new-windows.ps1
@@ -1,7 +1,7 @@
 runas /noprofile /user:Administrator Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopWheel -EA 0 | ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopWheel 1 }
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 scoop bucket add extras
-scoop install 7zip vim openssh git grep touch wget gawk concfg pshazz stack python ruby nodejs nmap msys2
+scoop install 7zip vim openssh git grep touch wget gawk concfg pshazz stack python ruby nodejs msys2
 [environment]::setenvironmentvariable('GIT_SSH', (resolve-path (scoop which ssh)), 'USER')
 gem install rake
 stack setup
