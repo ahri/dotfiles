23bcee24c7d8ad3b10caa6346317fc10227a17d6
Adam Piper <adam@ahri.net>
2017-08-24 21:59:45 +0100

    Add in cloning of dotfiles. Remove some env stuff.

--- per-environment/bootstrap-new-windows.ps1
+++ per-environment/bootstrap-new-windows.ps1
@@ -1,11 +1,20 @@
 runas /noprofile /user:Administrator Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopWheel -EA 0 | ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopWheel 1 }
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 scoop bucket add extras
-scoop install 7zip vim openssh git grep touch curl wget gawk concfg pshazz atom stack python ruby nodejs nmap msys2
+scoop install 7zip vim openssh git grep touch wget gawk concfg pshazz stack python ruby nodejs nmap msys2
 [environment]::setenvironmentvariable('GIT_SSH', (resolve-path (scoop which ssh)), 'USER')
 gem install rake
-npm install -g elm
 stack setup
-stack install stylish-haskell ghc-mod
-apm install language-haskell haskell-ghc-mod ide-haskell-cabal ide-haskell autocomplete-haskell
 concfg import flat small
+
+mkdir %USERPROFILE%\.ssh
+cd %USERPROFILE%\.ssh
+ssh-keygen -b 4096 -f id_rsa
+cat id_rsa.pub
+Read-Host -Prompt 'Add to github...'
+
+mkdir %USERPROFILE%\repos
+cd %USERPROFILE%\repos
+git clone git@github.com:ahri/dotfiles.git
+cd dotfiles
+rake
