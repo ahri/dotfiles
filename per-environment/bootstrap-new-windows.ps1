3b6f5ba31cdc561e32577e26968a504dbdc1b879
Adam Piper <adam@ahri.net>
2017-08-24 20:52:06 +0100

    Add mouse wheel flip.

--- per-environment/bootstrap-new-windows.ps1
+++ per-environment/bootstrap-new-windows.ps1
@@ -1,3 +1,4 @@
+runas /noprofile /user:Administrator Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopWheel -EA 0 | ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopWheel 1 }
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 scoop bucket add extras
 scoop install 7zip vim openssh git grep touch curl wget gawk concfg pshazz atom stack python ruby nodejs nmap msys2
