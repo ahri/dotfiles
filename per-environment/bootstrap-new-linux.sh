c4289b11e867a43db5fa4c14b955042fab42697f
Adam Piper <adam@ahri.net>
2018-02-13 19:07:27 +0000

    Add extra bits to smooth the bootstrapping process.

--- per-environment/bootstrap-new-linux.sh
+++ per-environment/bootstrap-new-linux.sh
@@ -5,8 +5,8 @@
 set -ue
 
 ubuntu="apt-transport-https ca-certificates software-properties-common"
-misc="htop curl wget lshw man less openssh-client"
-build="build-essential cmake git"
+misc="htop curl wget lshw man less openssh-client nmap"
+build="build-essential cmake git libtinfo-dev zlib1g-dev"
 languages="python-pip python3-pip ruby ruby-dev"
 docker="docker.io docker-compose"
 console_dev="vim dvtm"
@@ -29,9 +29,12 @@ sudo gem install rake neovim
 sudo pip3 install neovim
 
 curl https://nixos.org/nix/install | sh
+. ~/.nix-profile/etc/profile.d/nix.sh
 nix-env -i stack
 
 stack install intero hoogle hlint hspec stylish-haskell
+export PATH="$HOME/.local/bin:$PATH"
+hoogle generate
 
 # https://blog.g3rt.nl/upgrade-your-ssh-keys.html
 ssh-keygen -o -a 100 -t ed25519 -f "$HOME/.ssh/id_ed25519"
@@ -46,6 +49,10 @@ git config --global user.email "adam@ahri.net"
 git config --global user.name "Adam Piper"
 git clone git@github.com:ahri/dotfiles.git
 cd dotfiles
+rm -f ~/.gitconfig
 rake
+
+echo '. "$HOME/.profile_extra"' >> ~/.profile
+echo '. "$HOME/.bash_extra"' >> ~/.bashrc
 
 rake -T
